ebda4803f064f6b6ade0d68d58c7a9e86792990d
Mathieu Delsaut <mathieu.delsaut@univ-reunion.fr>
2022-09-16 09:35:25 +0400

    Add function to register

--- R/login.R
+++ R/login.R
@@ -1,3 +1,31 @@
+#' Register to SolarDB with an email
+#'
+#' This function allow to users to send an email address and obtain a token
+#' needed to authenticate user in rSolarDB.
+#'
+#' @param email character. The email of user
+#'
+#' @return NULL
+#' @export
+#'
+#' @author Mathieu Delsaut, \email{mathieu.delsaut@@univ-reunion.fr}
+#'
+#' @examples
+#' \dontrun{
+#'  register(email = "email@domain.com")
+#' }
+#'
+register <- function(email){
+
+  if (!is.character(email))
+    stop("email should be a character")
+
+  url <- paste0(.baseURL, "register?email=", email)
+  url %>% .getJSON
+  return(invisible(NULL))
+}
+
+
 #' Login to SolarDB with token
 #'
 #' This function allow to login to SolarDB and store the cookie needed to
